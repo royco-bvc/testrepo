2fde895f2355b09d37e3d40b1b930b1894b41825
royco-bvc <cokpokwasili@bowvalleycollege.ca>
2025-01-31 14:37:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name='Distutils',
-      version='1.0',
-      description='Python Distribution Utilities',
-      author='Greg Ward',
-      author_email='gward@python.net',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
-     )
+user_input = input("Enter your username: ")
+query = "SELECT * FROM users WHERE username = '" + user_input + "';"
+execute_query(query)  # This can be exploited
